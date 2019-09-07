14a71f34b7b9b284f41cfaf20f9946073ca4ace9
Jacob Moss <jacobmoss09@gmail.com>
2019-09-07 07:30:38 -0500

    successful database connection

--- app/models/burger.js
+++ app/models/burger.js
@@ -2,26 +2,17 @@ var orm = require("../config/orm");
 import Sequelize from 'sequelize';
 import sequelize from '../config/connection'
 
-var burger = {
-  all: function(cb) {
-    burger.findAll().then(burgers =>res.json(burgers));
-    // orm.all("burgers", function(res) {
-    //   cb(res);
-    // });
-  },
-  create: function(name, cb) {
-    // orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
-    burger.create({
-      burgername: Sequelize.STRING,
-      devoured: false
-    })
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
+class Burger extends Model{}
+Burger.init({
+  burger_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: false},
+  devoured: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    len: [5,40]
   }
-};
+}), {sequelize, modelName: 'burger'}
 
-module.exports = burger;
+export default Burger;
--- server.js
+++ server.js
@@ -2,25 +2,25 @@ var express = require("express");
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controller");
-
-app.use(routes);
 
 // listen on port 3000
 var PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+// require("./app/routes/api-routes.js")(app);
+// require("./app/routes/html-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
